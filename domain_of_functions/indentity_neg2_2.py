7de5ef9627fca0b46b301f0e817f801195a187b5
lumpofice <93841699+lumpofice@users.noreply.github.com>
2022-07-04 17:49:10 -0400

    initializing the repository in the previous synchronization. in the current sync: adding some material to the domains_of_functions.tex file, including a python script that generates two tables of values we use to plot a graph in our .tex file

---
+++ domain_of_functions/indentity_neg2_2.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+"""We will construct f(x) = x on
+[-2, 2]. Then, we will output the domain and range
+data to .table files that LaTeX will use to graph f. Additionally, to highlight
+the domain, we will plot a reflection onto the x-axis."""
+
+
+# Here is our function
+f = lambda u: u
+
+
+# Here is the domain of our function
+xs = [
+    np.arange(-2, 2, 0.01)
+    ]
+
+
+# Here is how we use the domain and the function definition to generate our
+# .table files for LaTeX
+for i, j in enumerate(xs):
+    with open('identity_neg2_2.table', 'w') as file:
+        for u in j:
+            if f(u) >= -2 and f(u) <= 2:
+                file.write('{:f} {:f}\n'.format(u, f(u)))
+        
+
+# Function for plotting the reflection onto the x-axis
+dom_f = lambda v: 0
+
+
+# Here is the domain of our function
+dom_xs = [
+    np.arange(-2, 2, 0.01)
+    ]
+
+
+# Here is how we use the domain and the function definition to generate our
+# .table files for LaTeX
+for s, t in enumerate(dom_xs):
+    with open('domain_identity_neg2_2.table', 'w') as file:
+        for v in t:
+            if v >= -2 and v <= 2:
+                file.write('{:f} {:f}\n'.format(v, f(v)))
